28da688d1adbaf7d12a11823ef0c813bac2fa39d
Balfagor <olegbeltion@gmail.com>
2021-01-12 21:17:35 +0300

    add BaseView/Presenter, AddCityPresenter/View methods

--- app/src/main/java/com/example/weathermvp/business/AddCityLogic.kt
+++ app/src/main/java/com/example/weathermvp/business/AddCityLogic.kt
@@ -1,2 +1,16 @@
 package com.example.weathermvp.business
 
+//  AddCity activity used for get city weather from city name
+//  and save it in Room
+
+interface AddCityPresenter : BasePresenter {
+    fun initV(v: AddCityView)
+    fun onBtnClick()
+}
+
+interface AddCityView : BaseVIew {
+    fun getCityName() : String
+    fun getStringFromID(stringID: Int) : String
+    fun showToast(str: String)
+    fun startNewActivity()
+}
--- app/src/main/java/com/example/weathermvp/business/BaseLogic.kt
+++ app/src/main/java/com/example/weathermvp/business/BaseLogic.kt
@@ -1,2 +1,10 @@
 package com.example.weathermvp.business
 
+interface BasePresenter{
+    fun inCreateView()
+}
+
+interface BaseVIew{
+    fun initViews()
+    fun initLogicItem()
+}
