0ec0fa15690e016af7dadbb482e588211b8f2531
Balfagor <olegbeltion@gmail.com>
2021-01-13 22:42:18 +0300

    add AddCity implement AppCompatActivity, AddCityView

--- app/src/main/java/com/example/weathermvp/presentation/addcity/AddCityActivity.kt
+++ app/src/main/java/com/example/weathermvp/presentation/addcity/AddCityActivity.kt
@@ -1,4 +1,76 @@
 package com.example.weathermvp.presentation.addcity
 
-class AddCityActivity {
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.weathermvp.R
+import com.example.weathermvp.business.AddCityView
+
+class AddCityActivity :
+        AppCompatActivity(),
+        AddCityView{
+
+    private lateinit var content: ConstraintLayout
+    private lateinit var progress: ProgressBar
+    private lateinit var cityName: EditText
+    private lateinit var getCityBtn: Button
+    private lateinit var cancelAdd: Button
+
+    private lateinit var presenter: AddCityPresenterImpl
+
+    override fun onStart() {
+        super.onStart()
+        initLogicItem()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.add_city)
+        initViews()
+    }
+
+    override fun getCityName(): String
+            = cityName.text.toString()
+
+    override fun getStringFromID(stringID: Int): String
+            = getString(stringID)
+
+    override fun showToast(str: String) {
+        Toast.makeText(applicationContext, str, Toast.LENGTH_SHORT).show()
+    }
+
+    override fun startNewActivity() {
+        TODO("Not yet implemented")
+    }
+
+    override fun initViews() {
+        content = findViewById(R.id.content_add_city)
+        progress = findViewById(R.id.progressbar)
+
+        cityName = findViewById(R.id.city_et_add_city)
+        getCityBtn = findViewById(R.id.get_city_btn_add_city)
+        cancelAdd = findViewById(R.id.cancel_city_btn_add_city)
+
+        presenter = AddCityPresenterImpl()
+        presenter.initV(this)
+    }
+
+    override fun initLogicItem() {
+        presenter.onCreateView()
+    }
+
+    override fun showContent() {
+        content.visibility = View.VISIBLE
+        progress.visibility = View.INVISIBLE
+    }
+
+    override fun hideContent() {
+        content.visibility = View.INVISIBLE
+        progress.visibility = View.VISIBLE
+    }
 }
