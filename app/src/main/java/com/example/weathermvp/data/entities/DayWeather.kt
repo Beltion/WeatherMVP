a4783ea36327bf49635650c3efb6a684494158d9
Balfagor <olegbeltion@gmail.com>
2021-01-21 11:03:57 +0300

    add DayWeather data class for retrofit Gson parse

---
+++ app/src/main/java/com/example/weathermvp/data/entities/DayWeather.kt
@@ -0,0 +1,13 @@
+package com.example.weathermvp.data.entities
+
+data class DayWeather(
+    val cod: Int,
+    val message: String,
+    val name: String,
+    val weather: ArrayList<Weather>,
+    val main: WeatherInDetail,
+    val clouds: Clouds,
+    val wind: Wind,
+    val snow: Snow?,
+    val rain: Rain?
+)
---
+++ app/src/main/java/com/example/weathermvp/data/entities/WeatherChildDataClass.kt
@@ -0,0 +1,42 @@
+package com.example.weathermvp.data.entities
+
+import com.google.gson.annotations.SerializedName
+
+data class Weather(
+        val id: Int,
+        val main: String,
+        val description: String,
+        val icon: String
+)
+
+data class WeatherInDetail(
+        val temp: Float,
+        val feels_like: Float,
+        val temp_min: Float,
+        val temp_max: Float,
+        val pressure: Float,
+        val humidity: Int
+)
+
+data class Wind(
+        val speed: Float,
+        val deg: Int
+)
+
+data class Rain(
+        @SerializedName("1h")
+        val atHour: Float,
+        @SerializedName("3h")
+        val at3Hour: Float
+)
+
+data class Snow(
+        @SerializedName("1h")
+        val atHour: Float,
+        @SerializedName("3h")
+        val at3Hour: Float
+)
+
+data class Clouds(
+        val all: Int
+)
